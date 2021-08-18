948d9b3de04a65ae3bcd7183e97996a0b51efb7a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-08-18 03:53:34 +0100

    Implement print logic

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 """
+ _   _  _____  __      __      ___   ___  ____  ____  ____  ____ 
+( )_( )(  _  )(  )    /__\    / __) / __)(  _ \(_  _)(  _ \(_  _)
+ ) _ (  )(_)(  )(__  /(__)\   \__ \( (__  )   / _)(_  )___/  )(  
+(_) (_)(_____)(____)(__)(__)  (___/ \___)(_)\_)(____)(__)   (__) 
 
-
+version 0.1.0
+author: Gbenga Adeyi <adeyigbenga005@gmail.com>
 """
 
 
-def main():
-    with open("data.txt","w") as f:
+def main() -> None:
+    with open("data.txt","r") as f:
         print(f.read())
 
 
